5995c6805e5f3aa1497c4487e1fab766c2c21454
allen <boseallen192@gmail.com>
2025-07-05 17:35:37 +0530

    Add project files.

---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Shiemi")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Shiemi.Pages")]
