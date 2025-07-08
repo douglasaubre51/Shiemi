8f4f327b6059e2899365e294c76bb4de5aa4399c
allen <boseallen192@gmail.com>
2025-07-08 14:10:06 +0530

    added user validation in signup vm

---
+++ Models/User.cs
@@ -0,0 +1,19 @@
+﻿using Shiemi.Attributes;
+
+namespace Shiemi.Models
+{
+    public class User
+    {
+        [UserValidation]
+        public string FirstName { get; set; }
+
+        [UserValidation]
+        public string LastName { get; set; }
+
+        [UserValidation]
+        public string Email { get; set; }
+
+        [UserValidation]
+        public string Password { get; set; }
+    }
+}
--- ViewModels/SignUpVM.cs
+++ ViewModels/SignUpVM.cs
@@ -1,10 +1,69 @@
-﻿namespace Shiemi.ViewModels
+﻿using CommunityToolkit.Mvvm.Input;
+using Shiemi.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+
+namespace Shiemi.ViewModels
 {
     public partial class SignUpVM : BaseVM
     {
+        // form binders
+        [ObservableProperty]
+        string? firstName;
+        [ObservableProperty]
+        string? lastName;
+        [ObservableProperty]
+        string? email;
+        [ObservableProperty]
+        string? password;
+
         public SignUpVM()
         {
             Title = "create new account";
+        }
+
+        // for calling http post service
+        [RelayCommand]
+        async Task SendUserDetails()
+        {
+            User user = new User()
+            {
+                FirstName = FirstName,
+                LastName = LastName,
+                Email = Email,
+                Password = Password
+            };
+
+            var validationContext = new ValidationContext(user);
+            var validationResults = new List<ValidationResult>();
+
+            bool validationResult = Validator.TryValidateObject(
+                user,
+                validationContext,
+                validationResults,
+                true
+                );
+
+            Debug.WriteLine($"validation result: {validationResult}");
+
+            if (validationResult is false)
+            {
+                await Shell.Current.DisplayAlertAsync(
+                    "validation error",
+                    "enter all fields",
+                    "ok"
+                    );
+
+                foreach (var m in validationResults)
+                {
+                    Debug.WriteLine(m.ToString());
+                }
+
+                return;
+            }
+
+            Debug.WriteLine("success!");
+            return;
         }
     }
 }
