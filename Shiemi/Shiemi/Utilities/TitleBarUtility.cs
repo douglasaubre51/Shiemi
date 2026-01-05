ad0a325cfe54d509c1fd6daf1a5000f805a26c75
Allen Bose <boseallen192@gmail.com>
2026-01-05 20:55:52 +0530

    1. created client profile cards in DevHub 2. created my projects carousel in home page

---
+++ Shiemi/Shiemi/Utilities/TitleBarUtility.cs
@@ -0,0 +1,24 @@
+﻿namespace Shiemi.Utilities;
+
+public static class TitleBarUtility
+{
+    private static Window? _window;
+
+    public static void InitTitleBar(Window window)
+    {
+        var titleBar = new TitleBar
+        {
+            Title = "Shiemi"
+        };
+
+        _window = window;
+    }
+
+    public static void ChangeTitle(string newTitle)
+    {
+        _window!.TitleBar = new TitleBar
+        {
+            Title = newTitle
+        };
+    }
+}
